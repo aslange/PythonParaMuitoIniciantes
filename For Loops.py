aaff304c05e9d30302075e22bc8692fecaa38f47
aslange <aslange@gmail.com>
2021-09-17 23:44:00 -0300

    Initial Code Commit
    Very Basic Python Code commit.

---
+++ For Loops.py
@@ -0,0 +1,12 @@
+
+
+for x in range(0,5):
+    print("Valor de x é", x)
+
+nome = "augusto"
+for letra in nome:
+    print(letra)
+
+lista= ["tiago", 19, "portugual"]
+for valor in lista:
+    print(valor)
